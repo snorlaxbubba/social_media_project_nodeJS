a688c17d760c5050e6410335bc89f221dccc5790
Mike <myamauchi@my.bcit.ca>
2022-12-02 10:12:34 -0800

    added main layout and index functionality

--- app.js
+++ app.js
@@ -11,67 +11,57 @@ const mongoose = require("mongoose");
 const uri =
         "mongodb+srv://myamauchi:Charlie1@ssd-0.457s283.mongodb.net/social-media-project?retryWrites=true&w=majority";
 
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// Store a reference to the default connection
 const db = mongoose.connection;
-
+// Log once we have a connection to Atlas
 db.once("open", function () {
     console.log("Connected to Mongo");
 });
+// Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-db.on("error", console.error.bind(console, "MongoDB connection error:" ));
-
+// Set up our server
 const app = express();
 
+// Parse form data and JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use (
+// Set up session management
+app.use(
     require("express-session")({
-        secret: "Hello World",
-        resave: false,
-        saveUninitialized: false,
+    secret: "a long time ago in a galaxy far far away",
+    resave: false,
+    saveUninitialized: false,
     })
 );
 
-app.use(cookieParser());
+// Initialize passport and configure for User model
+app.use(passport.initialize());
+app.use(passport.session());
+const User = require("./models/User");
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-// // Parse cookies and attach them to the request object
-// app.use(cookieParser());
+// Set up EJS templating
+app.set("view engine", "ejs");
+// Enable layouts
+app.use(expressLayouts);
+// Set the default layout
+app.set("layout", "./layouts/main-layout");
 
-// // Initialize passport and configure for User model
-// app.use(passport.initialize());
-// app.use(passport.session());
-// const User = require("./models/User");
-// passport.use(new LocalStrategy(User.authenticate()));
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
+// Make views folder globally accessible
+app.set("views", path.join(__dirname, "views"));
 
-// // Set up EJS templating
-// app.set("view engine", "ejs");
-// // Enable layouts
-// app.use(expressLayouts);
-// // Set the default layout
-// app.set("layout", "./layouts/main-layout");
-
-// // Make views folder globally accessible
-// app.set("views", path.join(__dirname, "views"));
-
-// // Make the public folder accessible for serving static files
-// app.use(express.static("public"));
+// Make the public folder accessible for serving static files
+app.use(express.static("public"));
 
 // Index routes
 const indexRouter = require("./routers/indexRouter");
 app.use(indexRouter);
-
-// // User routes
-// const userRouter = require("./routers/userRouter");
-// app.use("/user", userRouter);
-
-// // Secure routes
-// const secureRouter = require("./routers/secureRouter");
-// app.use("/secure", secureRouter);
 
 // Start listening
 const port = process.env.PORT || 3003;

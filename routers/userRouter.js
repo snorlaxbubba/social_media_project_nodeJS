1e96a70ea2f67e03a58eeb57b5d10c60bd0341ce
Mike <myamauchi@my.bcit.ca>
2022-12-02 10:44:05 -0800

    Added secureRouter and UserRouter Functionality

---
+++ routers/userRouter.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const userRouter = express.Router();
+
+const UserController = require("../controllers/UserController");
+const User = require("../models/User");
+
+userRouter.get("/register", UserController.Register);
+userRouter.post("/register", UserController.RegisterUser);
+
+userRouter.get("/login", UserController.Login);
+userRouter.post("/login", UserController.LoginUser);
+
+userRouter.get("/logout", UserController.Logout);
+
+module.exports = userRouter;

b725b387693934b793e68b03b7ebcb9be8c9fa04
Mike <113997900+catscancode-portfolio@users.noreply.github.com>
2023-01-05 10:16:25 -0800

    header styling

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -21,7 +21,7 @@ userRouter.post("/edit/:username", UserController.EditProfile);
 
 userRouter.get("/logout", UserController.Logout);
 
-userRouter.get("/profile/:id", UserController.Profile);
+userRouter.get("/profile", UserController.Profile);
 
 userRouter.get("/profiles", UserController.Profiles);
 
